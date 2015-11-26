cd9047ceb6acf144b04bdbb27580fee800181b68
Sojin Park <sojin8180@gmail.com>
2015-11-26 03:32:08 -0800

    modify addDLL function by sojin

--- addDLL_sojin.c
+++ addDLL_sojin.c
@@ -43,21 +43,26 @@ typedef struct ForT
 	struct ForPrint *prev;
 }ForT;
 
-
 void addDLL(char *name, int num)
 {
-	ForPrint *fresh;
-	fresh = (ForPrint *)malloc(sizeof(ForPrint));
-	fresh->next = fresh->prev = NULL;
-	strcpy(fresh->name, name);
-	fresh->real = num;
-
-	ForPrint *temp = head;
-	while (temp->next != NULL)
-	{
-		temp = temp->next;
-	}
-	temp->next = fresh;
-	fresh->prev = temp;
-	return;
+ ForPrint *fresh;
+ fresh = (ForPrint *)malloc(sizeof(ForPrint));
+ fresh->next = fresh->prev = NULL;
+ strcpy(fresh->name, name);
+ fresh->real = num;
+ if (head == NULL)
+ {
+  head = fresh;
+ }
+ else
+ {
+  ForPrint *temp = head;
+  while (temp->next != NULL)
+  {
+   temp = temp->next;
+  }
+  temp->next = fresh;
+  fresh->prev = temp;
+  return;
+ }
 }
