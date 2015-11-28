eb34386ae28ec4110e94b52a7516fb2dd5cecea2
leesangeun <qufwkfl3024@naver.com>
2015-11-28 08:00:28 -0800

    Load+addEdge+addweight function by sangeun

--- Load_sangeun.c
+++ Load_sangeun.c
@@ -77,6 +77,53 @@ void addEdge(char *identity, char *name, char *kind)  // 구분은 호선이랑.
 		fresh->prev = cur;
 	}
 }
+void addWeight(char *identity1, char *identity2, int dist) // 소요거리를 업데이트
+{
+	// step1. map에서 identity1을 찾고
+	// step2. identity2를 찾은 후
+	// stemp3. 두개를 연결.
+	mysubway *man1 = map, *man2 = 0;
+	mysubway *temp = map;
+	while (temp != NULL)
+	{
+		if (strcmp(identity1, temp->identity) == 0)
+		{
+			man1 = temp;
+			break;
+		}
+		temp = temp->next;
+	}
+	temp = map;
+	while (temp != NULL)
+	{
+		if (strcmp(identity2, temp->identity) == 0)
+		{
+			man2 = (mysubway *)malloc(sizeof(mysubway));
+			man2->connect = 0;
+			strcpy(man2->identity, temp->identity);
+			strcpy(man2->kind, temp->kind);
+			strcpy(man2->name, temp->name);
+			break;
+		}
+		temp = temp->next;
+	}
+	// 이제 연결..
+	if (man1->connect == NULL)
+	{
+		man1->connect = man2;
+		man1->connect->weight = dist;
+	}
+	else
+	{
+		temp = man1->connect;
+		while (temp->connect != NULL)
+		{
+			temp = temp->connect;
+		}
+		temp->connect = man2;
+		temp->connect->weight = dist;
+	}
+}
 void Load(void)
 {
  // 최초 한 번만 읽어오므로 전역변수일 필요 없음
@@ -106,7 +153,7 @@ void Load(void)
  {
   escape = fscanf(connectload,"%s%s%d",identity,identity2,&dist);
   if(escape == EOF){break;}
-  //addWeight();
+  addWeight(identity, identity2, dist);
  }
  fclose(dataload);
  fclose(connectload);
