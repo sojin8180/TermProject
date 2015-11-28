f2a7ee48d3a8a66bec77c330adfeb73f9a349243
leesangeun <qufwkfl3024@naver.com>
2015-11-28 07:57:57 -0800

    Load+addEdge function by sangeun

--- Load_sangeun.c
+++ Load_sangeun.c
@@ -42,11 +42,40 @@ typedef struct ForT
  struct ForPrint *prev;
 }ForT;
 
+mysubway *map = NULL;
+ForT *first = NULL;
 void Load(void); // 지하철 정보를 얻어올 함수
 
 int main(void)
 {
   Load();
+}
+void addEdge(char *identity, char *name, char *kind)  // 구분은 호선이랑.. 연결관계를 추가.
+{
+	mysubway *fresh = (mysubway *)malloc(sizeof(mysubway));
+	strcpy(fresh->identity, identity);
+	strcpy(fresh->name, name);
+	strcpy(fresh->kind, kind);
+	fresh->next = fresh->prev = NULL;
+	fresh->connect = NULL;
+	fresh->weight = 0;
+	// 판단은 kind로 결정.
+	//addDlist();
+
+	if (map == NULL)  // 맨 처음붙는 경우
+	{
+		map = fresh;
+	}
+	else  // 이미 붙어있는 것이 있는 경우,
+	{
+		mysubway *cur = map;
+		while (cur->next != 0) // 맨 끝을 찾아서 
+		{
+			cur = cur->next;
+		}
+		cur->next = fresh;
+		fresh->prev = cur;
+	}
 }
 void Load(void)
 {
@@ -71,7 +100,7 @@ void Load(void)
  {
   escape = fscanf(dataload,"%s%s%s",identity,name,kind);
   if(escape == EOF){break;}
-  //addEdge();
+  addEdge(identity, name, kind);
  }
  while(1)
  {
