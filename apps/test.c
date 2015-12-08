a95f3e164592507459e2949a2d213c646999a46b
Sojin Park <sojin8180@gmail.com>
2015-12-08 02:28:22 -0800

    library apps func by sojin & sangeun

---
+++ apps/test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <memory.h>
+#include <myheader.h>
+
+int main(void)
+{
+ while (1)
+ {
+  Load();
+  Mainmenu();
+  startrans = 0;
+  transflag = 0;
+  Dlist = NULL;
+  map = NULL;
+  head = NULL;
+  first = NULL;
+ }
+ return 0;
+}
+
+
